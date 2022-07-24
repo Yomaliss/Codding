faae1138144f863840a50e97df5fbd4108dc79ea
Yomaliss <mrartem200@mail.ru>
2022-07-24 21:37:28 +0700

    Поправка в задании 10.2 Homework Module 5

--- Homework Module 5/task 10.2.py
+++ Homework Module 5/task 10.2.py
@@ -1,7 +1,9 @@
 hour = int(input('Введите время в часах: '))
 if 14 <= hour < 15 \
         or 10 <= hour < 12 \
-        or 18 <= hour < 20:
+        or 18 <= hour < 20 \
+        or 0 <= hour < 8 \
+        or 22 <= hour <= 23:
     print('Посылку нельзя забрать')
 else:
     print('Посылку можно забрать')
