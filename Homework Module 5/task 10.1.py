c3a36563c0b3bfe20b505d051fddacf530e08918
Yomaliss <mrartem200@mail.ru>
2022-07-24 21:31:42 +0700

    Оптимизация 10 задания Homework Module 5

--- Homework Module 5/task 10.1.py
+++ Homework Module 5/task 10.1.py
@@ -1,8 +1,8 @@
-hour = int(input('Введите время в часах: '))  # Число от 0 до 23
-bad_hours = 14 <= hour <= 15 \
-         or 10 <= hour <= 12 \
-         or 18 <= hour <= 20
-if 8 <= hour <= 22 and not bad_hours:
+hour = int(input('Введите время в часах: '))
+if 8 <= hour < 22 and not \
+        (14 <= hour < 15
+         or 10 <= hour < 12
+         or 18 <= hour < 20):
     print('Посылку можно забрать')
 else:
     print('Посылку нельзя забрать')
--- Homework Module 5/task 10.2.py
+++ Homework Module 5/task 10.2.py
@@ -1,8 +1,7 @@
-hour = int(input('Введите время в часах: '))  # Число от 0 до 23
-bad_hours = 14 <= hour <= 15 \
-            or 10 <= hour <= 12 \
-            or 18 <= hour <= 20
-if bad_hours:
+hour = int(input('Введите время в часах: '))
+if 14 <= hour < 15 \
+        or 10 <= hour < 12 \
+        or 18 <= hour < 20:
     print('Посылку нельзя забрать')
 else:
     print('Посылку можно забрать')
