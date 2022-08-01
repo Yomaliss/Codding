e4fd0ab605ca96ecf362a14b6bc329f09f2a8a16
Yomaliss <mrartem200@mail.ru>
2022-08-01 23:26:49 +0700

    HM 10 hotfix

--- Homework Module 10/task 4.py
+++ Homework Module 10/task 4.py
@@ -3,9 +3,7 @@ size = int(input('Введите число: '))
 
 for row in range(size + 1):
     for col in range(size + 1):
-        if row == col:
-            print('^', end='')
-        if col == -row + size:
+        if row == col or col == -row + size:
             print('^', end='')
         else:
             print(' ', end='')
